da04386888d8e6bd05d45ca901ef08b51ac6ec41
神谷友樹 <jetree900@gmail.com>
2019-10-26 23:37:34 +0900

    Modelの設定

--- app/Condition.php
+++ app/Condition.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Condition extends Model
 {
-    //
+    protected $fillable = ['machine_id','temp','water_temp'];
+
+    public function machine(){
+        return $this->belongsTo('App\machine');
+    }
 }
--- app/Machines.php
+++ app/Machines.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Machines extends Model
 {
-    //
+    protected $fillable = ['machine_name'];
+
+    public function conditions(){
+        return $this->hasMany('App\Condition');
+    }
 }

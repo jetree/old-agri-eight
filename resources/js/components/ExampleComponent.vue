6d29cf5b1c0e1ef54a2bba1066a60ee4bfd5deb6
神谷友樹 <jetree900@gmail.com>
2019-10-27 23:14:41 +0900

    テストようにconponentを変更 axiosを使ってデータを取得を確認

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -2,11 +2,18 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card mb-3">
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
                         I'm an example component.
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-header">API TEST</div>
+
+                    <div class="card-body">
+                        {{ $data }}
                     </div>
                 </div>
             </div>
@@ -16,8 +23,12 @@
 
 <script>
     export default {
+        data: function(){
+            return $data
+        },
         mounted() {
-            console.log('Component mounted.')
+            $data = axios.get('/api/test')
+            console.log($data)
         }
     }
 </script>

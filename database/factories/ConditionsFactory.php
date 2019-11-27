921b1a269c3df7715d58f96baeefa800f26e9990
神谷友樹 <jetree900@gmail.com>
2019-11-27 22:38:57 +0900

    テスト用のseederを実施

--- database/factories/ConditionsFactory.php
+++ database/factories/ConditionsFactory.php
@@ -5,8 +5,11 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Condition::class, function (Faker $faker) {
     return [
-        //
+        'machine_id' => '1',
+        'temp' => $faker->numberBetween($min = 5,$max = 35),
+        'water_temp' => $faker->numberBetween($min = 5,$max = 35),
+        'created_at' => $faker->datetime($max = 'now', $timezone = date_default_timezone_get()),
     ];
 });
